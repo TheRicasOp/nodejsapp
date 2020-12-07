7ed38949974eb09eb0dbe906368cc14ed32f758c
TheRicasOp <ricardodavila26@hotmail.com>
2020-12-06 20:16:42 -0600

    caliz 01

--- index.js
+++ index.js
@@ -1,27 +1,56 @@
-// index.js
-
 /**
  * Required External Modules
  */
 const express = require("express");
 const path = require("path");
+const morgan = require('morgan');
+const body_parser = require("body-parser");
+const cors = require('cors');
+const admin = require('firebase-admin');
 
 /**
  * App Variables
  */
 const app = express();
 const port = process.env.PORT || "8000";
+var serviceAccount = require('infinachat-firebase-adminsdk-n6u87-8365e8e835.json');
 
 /**
  *  App Configuration
  */
+ app.use(cors());
+ app.use(body_parser.urlencoded({ extended: false }));
+ app.use(body_parser.json());
+
+ admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
 
 /**
  * Routes Definitions
  */
+//Prueba
 app.get("/", (req, res) => {
     res.status(200).send("WHATABYTE: Food For Devs");
   });
+
+//getUsers
+app.get('/getUsers',(req,res) => {
+    let data = [];
+  db.collection("users").get().
+    then((snapshot) => {
+        snapshot.forEach((doc) => {
+            data.push(doc.data().username);
+        });
+            
+            res.send(data);
+        })
+    .catch((err) => {
+        console.log("Error getting documents", err);
+    });
+});
 
 /**
  * Server Activation
