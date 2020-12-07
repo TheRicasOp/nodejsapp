a8ab81c7a472e1d03381bbed1d19a22612a3790e
TheRicasOp <ricardodavila26@hotmail.com>
2020-12-06 20:36:21 -0600

    caliz 01 otra vez

--- index.js
+++ index.js
@@ -3,30 +3,16 @@
  */
 const express = require("express");
 const path = require("path");
-const morgan = require('morgan');
-const body_parser = require("body-parser");
-const cors = require('cors');
-const admin = require('firebase-admin');
 
 /**
  * App Variables
  */
 const app = express();
 const port = process.env.PORT || "8000";
-var serviceAccount = require('./infinachat-firebase-adminsdk-n6u87-8365e8e835.json');
 
 /**
  *  App Configuration
  */
- app.use(cors());
- app.use(body_parser.urlencoded({ extended: false }));
- app.use(body_parser.json());
-
- admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-
-const db = admin.firestore();
 
 /**
  * Routes Definitions
@@ -37,20 +23,6 @@ app.get("/", (req, res) => {
   });
 
 //getUsers
-app.get('/getUsers',(req,res) => {
-    let data = [];
-  db.collection("users").get().
-    then((snapshot) => {
-        snapshot.forEach((doc) => {
-            data.push(doc.data().username);
-        });
-            
-            res.send(data);
-        })
-    .catch((err) => {
-        console.log("Error getting documents", err);
-    });
-});
 
 /**
  * Server Activation
