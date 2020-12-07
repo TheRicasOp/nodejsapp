30cd95f8656876577644474bee1e430e9e0c2d62
TheRicasOp <ricardodavila26@hotmail.com>
2020-12-06 20:42:44 -0600

    prueba02

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
  */
 const express = require("express");
 const path = require("path");
-const morgan = require('morgan');
 const body_parser = require("body-parser");
 const cors = require('cors');
 const admin = require('firebase-admin');
